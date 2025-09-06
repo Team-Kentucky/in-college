c571901b44c762a641496a13faf4150b7dbfd848
Matthew West <matthewjwest53@gmail.com>
2025-09-06 17:53:06 -0400

    Added basic file IO code

--- src/inCollege.cob
+++ src/inCollege.cob
@@ -1,8 +1,32 @@
-*> This is free-form
 IDENTIFICATION DIVISION.
-PROGRAM-ID. INCOLLEGE.
+program-id. INCOLLEGE.
+
+
+*>###################################################################
 ENVIRONMENT DIVISION.
+
+input-output section.
+file-control.
+       select input-file assign to 'input.txt'
+           organization is line sequential
+           file status is input-file-status.
+
+
+*>###################################################################
 DATA DIVISION.
+
+file section.
+fd input-file.
+01 input-line pic x(100). *> Need to decide on resonable size for lines
+
+working-storage section.
+01 input-file-status pic xx.
+01 eof pic x value 'N'.
+
+local-storage section.
+
+
+*>###################################################################
 PROCEDURE DIVISION.
-    DISPLAY "Hello from GNU COBOL in Docker + VS Code!".
-    STOP RUN.
+
+       stop run.
