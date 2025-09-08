43385c90cfdf588efaa23a8fb7ff59e7de426a09
Matthew <matthewjwest53@gmail.com>
2025-09-08 12:15:43 -0400

    Added ability to accept inputs with a prompt and now outputting inputs as if user had typed

--- src/inCollege.cob
+++ src/inCollege.cob
@@ -30,14 +30,14 @@ fd output-file.
 
 working-storage section.
 *>-----readInputLine variables-----
+01 input-prompt pic x(100).
 01 input-file-status pic xx.
 
 *>-----outputLine variables-----
 01 output-buffer pic x(150).
-01 output-newline pic x.
-       88 yes-newline value 'Y'.
-       88 no-newline  value 'N'.
 01 output-file-status pic xx.
+
+*>-----logIn variables-----
 
 local-storage section.
 
@@ -47,13 +47,92 @@ PROCEDURE DIVISION.
 main.
        open input input-file.
 
-       perform displayStartUp.
+       perform displayLogo.
        perform logInScreen.
 
        close input-file.
        stop run.
 
-displayStartUp.
+
+
+logInScreen.
+       move "Create an account? [y\N]:" to input-prompt.
+       perform readInputLine.
+
+
+       exit.
+
+
+*> Paragraph: readInputLine
+*> Purpose:   Reads in the next line of the input file
+*> Input:     None
+*> Output:    input-buffer - Line from the file
+*> User is responsible for opening and closing the input file when using this
+readInputLine.
+       if input-file-status = "00"
+           read input-file
+               not at end
+                   string
+                       function trim(input-prompt, trailing) delimited by size
+                       " " delimited by size                                   *> Will always have an extra space (even if input has no prompt)
+                       function trim(input-buffer, trailing) delimited by size
+                       into output-buffer
+                   end-string
+                   perform outputLine
+               at end
+                   *> Input-buffer is stale now, so set to spaces
+                   move spaces to input-buffer
+                   *> Notify user
+                   move "Reached end of input file ( ੭ˊᵕˋ)੭" to output-buffer
+                   perform outputLine
+           end-read
+       else
+           string
+               "Error reading input file: " delimited by size
+               input-file-status            delimited by size
+               " (｡•́︿•̀｡)"                   delimited by size
+               into output-buffer
+           end-string
+           perform outputLine
+       end-if
+       exit.
+
+
+*> Paragraph: outputLine
+*> Purpose:   Prints string in buffer to console and saves to output log
+*> Input:     output-buffer - string you want to be output
+*> Output:    None
+outputLine.
+       open extend output-file.
+
+       if output-file-status = "35"
+           *> Log file does not exist yet, so create it
+           open output output-file
+       end-if
+
+       *> Ensure file opened properly
+       if output-file-status = "00"
+           *> If we want to ensure that the console and output file are the same,
+           *> console output can only happen if output file opens
+           move output-buffer to output-line
+           display function trim(output-line, trailing) *> Will make logged output have extra spaces compared to command line. Do we care?
+           write output-line
+
+           close output-file
+       else
+           display "Error opening output file: " output-file-status " (っ- ‸ - ς)"
+       end-if
+
+       *> Some characters get 'stuck', manually clearing fixes it though
+       move spaces to output-buffer.
+       exit.
+
+
+*> Paragraph: displayLogo
+*> Purpose:   Prints the beautiful ascii logo for us
+*> Input:     None
+*> Output:    None
+displayLogo.
        move "############################################################################################" to output-buffer.
        perform outputLine.
        perform outputLine.
@@ -86,72 +165,4 @@ displayStartUp.
        move "                                                                    Created by Team Kentucky" to output-buffer.
        perform outputLine.
        perform outputLine.
-       perform outputLine.
-       exit.
-
-logInScreen.
-       move "Create an account? [y\N]: " to output-buffer.
-       perform outputLine.
-       perform readInputLine.
-       move input-buffer to output-buffer.
-       perform outputLine.
-       exit.
-
-
-*> Paragraph: readInputLine
-*> Purpose:   Reads in the next line of the input file
-*> Input:     None
-*> Output:    input-buffer - Line from the file
-*> User is responsible for opening and closing the input file when using this
-readInputLine.
-       if input-file-status = "00"
-           read input-file
-               at end
-                   *> Input-buffer is stale now, so set to spaces
-                   move spaces to input-buffer
-                   *> Notify user
-                   move "Reached end of input file ( ੭ˊᵕˋ)੭" to output-buffer
-                   perform outputLine
-           end-read
-       else
-           string
-               "Error reading input file: " delimited by size
-               input-file-status            delimited by size
-               " (｡•́︿•̀｡)"                   delimited by size
-               into output-buffer
-           end-string
-           perform outputLine
-       end-if
-       exit.
-
-
-*> Paragraph: outputLine
-*> Purpose:   Prints string in buffer to console and saves to output log
-*> Input:     output-buffer - string you want to be output
-*> Output:    None
-outputLine.
-       open extend output-file.
-
-       if output-file-status = "35"
-           *> Log file does not exist yet, so create it
-           open output output-file
-       end-if
-
-       *> HAVE FLAG FOR IF WE WANT TO HAVE NEWLINE AT END OF INPUT
-       *> Ensure file opened properly
-       if output-file-status = "00"
-           *> If we want to ensure that the console and output file are the same,
-           *> console output can only happen if output file opens
-           display output-buffer
-
-           move output-buffer to output-line
-           write output-line
-
-           close output-file
-       else
-           display "Error opening output file: " output-file-status " (っ- ‸ - ς)"
-       end-if
-
-       *> Some characters get 'stuck', manually clearing fixes it though
-       move spaces to output-buffer.
        exit.
