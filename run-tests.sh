660141643d5c41960f35fe6a68bc700c32355d20
Matthew <matthewjwest53@gmail.com>
2025-09-15 15:55:38 -0400

    Added more edge case checking to testing script and support for provided test databases

--- run-tests.sh
+++ run-tests.sh
@@ -1,53 +1,91 @@
 #!/bin/bash
 
-cd bin
 line_count=0
 success_count=0
 
 #Clearning the testing enviornment
-if [ -f "output.txt" ]; then rm "output.txt"; fi
-if [ -f "test-output" ]; then rm -rf "test-output"; fi
-mkdir -p test-output
-printf "[%2d] %-45s [%1s]\n" $line_count "Setting up enviornment" "X"
+result="S"
+missing_binary=false
+if [ -d "bin" ]; then cd bin;
+else result="F"; fi
+if [ "$result" == "S" ]
+then
+    if [ -f "inCollege" ]
+    then :
+    else result="F"; missing_binary=true; fi
+    if [ -f "output.txt" ]; then rm "output.txt"; fi
+    if [ -d "test-output" ]; then rm -rf "test-output"; fi
+    mkdir -p test-output
+fi
+
+printf "[%2d] %-45s [%1s]\n" $line_count "Setting up enviornment" $result
 ((line_count++))
 
-# Running Test Suite
-for file in ../input-files/*; do
-    if [ -f "acct-database.dat" ]; then rm "acct-database.dat"; fi
-    if [ -f "input.txt" ]; then rm "input.txt"; fi
+if [ "$result" == "S" ];
+then
+    # Running Test Suite
+    for file in ../input-files/*.txt; do
+        if [ -f "acct-database.dat" ]; then rm "acct-database.dat"; fi
+        if [ -f "input.txt" ]; then rm "input.txt"; fi
 
-    file_no_path=$(basename "$file")
-    filename="${file_no_path%.*}"
-    output_file="test-output/$filename-output.txt"
+        file_no_path=$(basename "$file")
+        filename="${file_no_path%.*}"
+        output_file="test-output/$filename-output.txt"
 
-    result="F"
-    cp $file input.txt
-    ./inCollege > $output_file
-
-    expected_output="../output-files/$filename-output.txt"
-    if [ -f $expected_output ]
-    then
-        diff_from_expected=$(diff $output_file $expected_output)
-        if [$diff_from_expected == ""]
-        then
-            result="P"
-            ((success_count++))
+        # Find associated testing database if it exists
+        database="../input-files/$filename-database.dat"
+        if [ -f "$database" ]; then
+            if [ -f "acct-database.dat" ]; then rm "acct-database.dat"; fi
+            cp $database "acct-database.dat"
         fi
+
+        result="F"
+        cp $file input.txt
+        ./inCollege > $output_file
+
+        cat $output_file >> "test-output/cli-output.txt"
+
+        expected_output="../output-files/$filename-output.txt"
+        if [ -f $expected_output ]
+        then
+            diff_from_expected=$(diff $output_file $expected_output)
+            text_output=$(head $output_file )
+            if [ "$diff_from_expected" == "" ] && [ "$text_output" != "" ];
+            then
+                result="P"
+                ((success_count++))
+            fi
+        else
+            result="X"
+        fi
+
+        printf "[%2d] %-45s [%1s]\n" $line_count $filename $result
+        if [ $result == "X" ]; then printf "     %s\n" "Expected output file does not exist. Manual verification required"; fi
+        ((line_count++))
+    done
+
+    # Check if everything displayed to cli was properly logged
+    diff_from_cli=$(diff "test-output/cli-output.txt" "output.txt")
+
+    if [ "$diff_from_cli" == "" ]
+    then
+        result="P"
+        ((success_count++))
     else
-        result="X"
+        result="F"
     fi
+    printf "[%2d] %-45s [%1s]\n" $line_count "Command line matches output log" $result
 
-    printf "[%2d] %-45s [%1s]\n" $line_count $filename $result
-    if [ $result == "X" ]; then printf "     %s\n" "Expected output file does not exist. Manual verification required"; fi
-    ((line_count++))
-done
-((line_count--))
+    cp output.txt test-output
 
-cp acct-database.dat test-output
-cp output.txt test-output
-
-printf "\nTest suite was"; if [ $success_count == $line_count ]; then printf " sucessful. "; else printf " unsuccessful. "; fi
-printf "%d/%d tests passed\n" $success_count $line_count
-
+    printf "\nTest suite was"; if [ $success_count == $line_count ]; then printf " sucessful. "; else printf " unsuccessful. "; fi
+    printf "%d/%d tests passed\n" $success_count $line_count
+elif [ $missing_binary == true ]
+then
+    printf "     %s\n" "Program binary does not exist. Have you built it? (Try: make)"
+elif [ $missing_binary == false ]
+then
+    printf "     %s\n" "Bin folder does not exist. Have you built the program? (Try: make)";
+fi
 cd ..
 
